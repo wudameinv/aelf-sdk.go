95492db326ae138498a531040a860cb4e6cec584
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-24 20:23:40 +0800

    Update start.sh

--- scripts/aelf-node/start.sh
+++ scripts/aelf-node/start.sh
@@ -6,6 +6,6 @@ sudo sed -i "s/127.0.0.1/$ip/g" ../../test/client_test.go
 sudo sed -i "s/127.0.0.1/$ip/g" ../../test/network_test.go 
 sudo docker pull aelf/node:testnet-v0.9.2
 sudo docker run -itd --name aelf-node-test -v $dir:/opt/node -v $dir/keys:/root/.local/share/aelf/keys -p 8000:8000 -p 6801:6800 -w /opt/node aelf/node:testnet-v0.9.2 dotnet /app/AElf.Launcher.dll
-sleep 30
+sleep 60
 height=`curl -s http://$ip:8000/api/blockChain/blockHeight`
 echo "height is $height"
