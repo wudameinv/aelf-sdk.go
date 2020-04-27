cf0e14ef6fb67ce2f1811654c72d6787edb09885
wudameinv <53204268+wudameinv@users.noreply.github.com>
2020-04-27 15:19:39 +0800

    Update start-window.ps1

--- scripts/aelf-node/start-window.ps1
+++ scripts/aelf-node/start-window.ps1
@@ -1,16 +1,16 @@
-wget https://github.com/microsoftarchive/redis/releases/download/win-3.2.100/Redis-x64-3.2.100.zip -OutFile  redis.zip
-Expand-Archive -Path redis.zip -DestinationPath redis ;
-$job1 = Start-Job -ScriptBlock { cd D:\a\1\s\redis; .\redis-server.exe; }
-sleep 30
-mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
-cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
-wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip -OutFile  aelf.zip ;
-Expand-Archive -Path aelf.zip -DestinationPath aelf ;
-cp scripts\aelf-node\appsettings.json  aelf\aelf\appsettings.json ;
-cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf\appsettings.MainChain.TestNet.json ;
-cd aelf/aelf
-$job2 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf; dotnet AElf.Launcher.dll; } 
-sleep 60
+# wget https://github.com/microsoftarchive/redis/releases/download/win-3.2.100/Redis-x64-3.2.100.zip -OutFile  redis.zip
+# Expand-Archive -Path redis.zip -DestinationPath redis ;
+# $job1 = Start-Job -ScriptBlock { cd D:\a\1\s\redis; .\redis-server.exe; }
+# sleep 30
+# mkdir -p C:\Users\VssAdministrator\AppData\Local\aelf\keys
+# cp -r scripts\aelf-node\keys\* C:\Users\VssAdministrator\AppData\Local\aelf\keys;
+# wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip -OutFile  aelf.zip ;
+# Expand-Archive -Path aelf.zip -DestinationPath aelf ;
+# cp scripts\aelf-node\appsettings.json  aelf\aelf\appsettings.json ;
+# cp scripts\aelf-node\appsettings.MainChain.TestNet.json  aelf\aelf\appsettings.MainChain.TestNet.json ;
+# cd aelf/aelf
+# $job2 = Start-Job -ScriptBlock { cd D:\a\1\s\aelf\aelf; dotnet AElf.Launcher.dll; } 
+# sleep 60
 cd D:\a\1\s
 cd client/ 
 go build
@@ -23,4 +23,4 @@ go build
 cd ../protobuf/generated/
 go build
 cd ../../test
-
+C:\hostedtoolcache\windows\go\1.13.5\x64\bin\go.exe test
